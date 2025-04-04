88a75b4bbd576e7ba3ea11d2afb5a86c4dc65a54
PABLO VALENTIN <pablo.valentin@fatec.sp.gov.br>
2025-04-03 21:03:49 -0300

    feat: Implementando os eventos do JavaScript.

--- Assets/JS/Script.js
+++ Assets/JS/Script.js
@@ -1,9 +1,25 @@
+// Campos do formulário da página HTML.
+
+const form_fields = [
+
+    document.getElementById("user-name"),
+    document.getElementById("user-email"),
+    document.getElementById("user-password")
+
+];
+
+// Evento acionado toda vez que o botão "SUBMIT" do formulário for apertado.
+
 document.querySelector("form").onsubmit = function(event) {
 
     event.preventDefault();
 
     console.log("Envio do formulário abortado.");
 
+    alert(`\nNome informado: ${form_fields[0].value}.\n\nE-mail informado: ${form_fields[1].value}.\n\nSenha criada: ${form_fields[2].value}.`);
+
     this.reset();
+
+    console.clear();
 
 }
